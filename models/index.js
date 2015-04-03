4fb7f0aa7971eae3a3a1dff1e13ac9071cf4509f
kgilmur <kgilmur@gmail.com>
2015-04-03 13:56:07 -0700

    adding production db

--- models/index.js
+++ models/index.js
@@ -9,6 +9,19 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
+// NEEDED FOR HEROKU ///////////
+if(config.use_env_variable){
+  var db_info = process.env[config.use_env_variable].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  config.dialect=db_info[1];
+  config.username=db_info[2];
+  config.password=db_info[3];
+  config.host=db_info[4];
+  config.port=db_info[5];
+  config.database=db_info[6];
+}
+//////////////////////////////
+
+
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
